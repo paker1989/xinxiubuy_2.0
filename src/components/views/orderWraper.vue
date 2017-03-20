b9071dfc769a813072a2e2efdc9f147e8d6aa3ba
paker1989 <xubinqz@gmail.com>
2017-03-20 18:36:30 +0100

    commit

--- src/components/views/orderWraper.vue
+++ src/components/views/orderWraper.vue
@@ -1,6 +1,12 @@
 <template>
   <div class="orderWraper">
-   谁是orderWraper,我是orderWraper
+   <div class="backToOrdersContainer">
+   	<icon name="chevron-left" class="icon"/>
+   	<router-link :to="'/orders'" class="text-nav clickable">回到订单</router-link>
+   </div><!--end backToOrdersContainer-->
+   <div>
+   	
+   </div>
   </div>
 </template>
 
@@ -18,5 +24,28 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
- 
+ .orderWraper{
+  position:relative;
+  width: 60%;
+  padding-top:30px;
+  margin:0 auto;
+
+  & .icon{
+   vertical-align: text-bottom;
+   color: lighten(black,40%);
+   margin:0 2px;
+   cursor: pointer;
+  }
+
+  & .clickable:hover{
+   cursor: pointer;
+   font-weight:600;
+   color:lighten(black,20%)
+  }
+
+  & .backToOrdersContainer{
+   position:relative;
+   text-align: left;
+  }
+ }
 </style>
