d863e00d08c7c83366f69484c7550c90ab6bdbda
parker1989 <xubinqz@gmail.com>
2017-03-20 23:58:52 +0100

    commit

---
+++ src/components/userProfileForOrder.vue
@@ -0,0 +1,122 @@
+<template>
+  <div class="userProfileForOrder">
+   <div class="userProfile-title">
+    <h1 class="text-nav bold">用户资料</h1>
+   </div><!--end userProfile title-->
+   <div class="userProfile-details" v-if="edit==false">
+    <tbody class="userProfile-details-table">
+     <tr>
+      <td class="bold">姓名</td>
+      <td class="text-content">喵喵</td>
+     </tr>
+     <tr>
+      <td class="bold">电话</td>
+      <td class="text-content">138170211157</td>
+     </tr>
+     <tr>
+      <td class="bold">邮寄地址</td>
+      <td class="text-content">辽宁省葫芦市龙湾区玉皇街道新区  凌云书苑小区物业办公室 石正 收</td>
+     </tr>
+     <tr>
+      <td class="bold">备注</td>
+      <td class="text-content">这个人比较难搞</td>
+     </tr>
+    </tbody>
+   </div><!--end userProfile-details-->
+   <div class="edit-userProfile">
+    
+   </div><!--end editing userProfile-->
+   <div class="action">
+    <span class="text-nav clickable edit" v-show="edit==false" @click="edit=true">编辑</span>
+    <span class="text-nav clickable edit" v-show="edit" @click="edit=false">保存</span>
+    <span class="text-nav clickable cancel" v-show="edit" @click="edit=false">取消</span>
+   </div><!--end action-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'userProfileForOrder',
+ 
+  props: [],
+
+  data () {
+    return {
+      edit    :  false
+    }
+  }
+
+}
+</script>
+
+<style lang="scss" scoped>
+ .userProfileForOrder{
+   position: relative;
+   width: 100%;
+   min-height: 60vh;
+   border-right:2px solid #F6F7FA;
+
+   & .userProfile-title{
+    width: 100%;
+    height: 80px;
+    display: flex;
+    align-items: center;
+    justify-content: flex-start; 
+    padding-left:20px;
+   }
+
+   & .userProfile-details{
+    width: 100%;
+
+    & .userProfile-details-table{
+     width: 100%;
+     margin-bottom: 20px;
+
+     & tr td{
+      height: 45px;
+      border-bottom:2px solid #F6F7FA;
+      line-height: 1.4em;
+     }
+
+     &  tr td:first-child{
+      width:60px;
+     }
+
+     & tr td:last-child{
+      padding:20px 30px;
+     }
+
+     & tr:last-child td{
+      border-bottom:none;
+     }
+    }
+   }/* end userProfile-details*/
+
+   & .action{
+    width: 100%;
+    display: flex;
+    padding-top:40px;
+    padding-right:40px;
+    justify-content: flex-end;
+    align-items:center;
+
+    & .clickable:hover{
+      cursor: pointer;
+      font-weight: 600;
+    }
+
+    & .edit, & .cancel{
+      margin-left:20px;
+    }
+
+    & .edit{
+     color:#6ba045;
+    }
+
+    & .cancel{
+     color:lighten(red,10%);
+    }
+   }/*end action*/
+ }
+ 
+</style>
--- src/components/views/orderWraper.vue
+++ src/components/views/orderWraper.vue
@@ -1,22 +1,40 @@
 <template>
   <div class="orderWraper">
-   <div class="backToOrdersContainer">
-   	<icon name="chevron-left" class="icon"/>
-   	<router-link :to="'/orders'" class="text-nav clickable">回到订单</router-link>
-   </div><!--end backToOrdersContainer-->
-   <div>
-   	
+   <div class="header">
+    <div class="backToOrdersContainer">
+     <icon name="chevron-left" class="icon"/>
+     <router-link :to="'/orders'" class="text-nav clickable">回到订单</router-link>
+    </div><!--end backToOrdersContainer--> 
+    <div class="orderStatusContainer">
+      <span :class="{'orderstatus':true, 'clickable':true, 'actived':selectedOrderStatus==1}"
+             @click="selectedOrderStatus=1">当前订单</span>
+      <span :class="{'orderstatus':true, 'clickable':true, 'actived':selectedOrderStatus==2}"
+             @click="selectedOrderStatus=2">已结束</span>
+    </div><!--end backToOrdersContainer--> 
+   </div><!--header-->
+   <div class="content">
+    <div class="userProfile">
+     <userProfile/>
+    </div>
+    <div class="orderContainer"></div>
+   </div><!--content-->
    </div>
   </div>
 </template>
 
 <script>
+import UserProfile from 'components/userProfileForOrder'
+
 export default {
   name: 'orderWraper',
 
+  components: {
+   UserProfile
+  },
+
   data () {
     return {
-      
+      selectedOrderStatus : 1// 1 for 当前订单 2 for 已归档
     }
   }
 }
@@ -26,26 +44,78 @@ export default {
 <style lang="scss" scoped>
  .orderWraper{
   position:relative;
-  width: 60%;
-  padding-top:30px;
-  margin:0 auto;
+  width: 70%;
+  margin:10px auto;
+  padding:30px 15px;
+  border-radius:5px;
+  border:1px solid #eee;
+  box-shadow: 0 0 10px rgba(0,0,0,.3);
+  
 
   & .icon{
    vertical-align: text-bottom;
    color: lighten(black,40%);
-   margin:0 2px;
+   margin:0 5px;
    cursor: pointer;
   }
 
   & .clickable:hover{
    cursor: pointer;
    font-weight:600;
-   color:lighten(black,20%)
   }
 
-  & .backToOrdersContainer{
+  & .header{
+    position:relative;
+    width: 100%;
+    height: 40px;
+    display: flex;
+    border-bottom:1px solid #eee;
+
+   & .backToOrdersContainer,& .orderStatusContainer{
+     position:relative;
+     text-align: left;  
+     display: flex;
+     align-items:center;
+   }
+
+   & .backToOrdersContainer{
+    width: 40%;
+   }
+
+   & .orderStatusContainer{
+    width: 60%;
+
+    & .orderstatus{
+     position:relative;
+     margin: 0 20px;
+     line-height: 30px;
+     transition:all 1s linear; 
+    }
+
+    & .actived:after{
+      content: '';
+      border-top:3px solid #ff5722;
+      width: 100%;
+      position:absolute;
+      bottom:-5px;
+      left:0;
+    }
+   }
+  }/*header end*/
+
+  & .content{
    position:relative;
-   text-align: left;
+   display: flex;
+
+   & .userProfile{
+    position:relative;
+    width: 30%;
+   }
+
+   & .orderContainer{
+    position:relative;
+    width: 70%;
+   }
   }
  }
 </style>
