717198a8c371d98ec610a77e3aca4ecc4ee0da61
parker1989 <xubinqz@gmail.com>
2017-03-22 23:58:50 +0100

    commit

--- src/components/orderCollapse.vue
+++ src/components/orderCollapse.vue
@@ -58,23 +58,35 @@
          <td class="text-content">0</td>
          <td class="clickable edit">编辑</td>
         </tr>
-  <!--
-        <tr>
-          <td colspan="6">
-            <custInput :placeholder="'new product'">
+        <tr v-if="addingProduct" class="newItemWraper">
+          <td colspan="2">
+            <custInput class="newItem" :placeholder="'产品名'">
+          </td>
+          <td>
+           <select class="comp-dropdown">
+            <option class="text-nav" v-for="i in 9">{{i}}</option>
+           </select>
+          </td>
+          <td>
+           <custInput class="newItem" :placeholder="'单价'">
+          </td>
+          <td>
+          </td>
+          <td >
+            <span class="clickable edit" @click="saveNew">保存</span>
+            <span class="clickable cancel" @click="cancelNew">取消</span>
           </td>
         </tr>
-    -->
-        <tr>
+        <tr v-if="!addingProduct">
           <td colspan="5"></td>
-          <td class="addNew">
-            <icon name="plus" class="text-icon" scale="1"/>
-            <span class="clickable">添加</span>
+          <td class="addNew" @click="addNew">
+            <!--<icon name="plus" class="text-icon" scale="1.3"/>-->
+            <button class="plus"></button>
           </td>
         </tr>
         <tr>
          <td colspan="4" class="text-nav">运费</td>
-         <td class="text-content" colspan="3">10</td>
+         <td class="text-content" colspan="3" style="text-align:left;">10</td>
 
         </tr>
         <tr>
@@ -86,13 +98,20 @@
       </table>
       <div class="commentActionContainer">
        <div class="commentWraper">
-         <span class="bold comment-label">备注:</span>
-         <span class="text-content">上次忘寄的化妆棉寄过去</span>
+         <span v-if="!isEditOrder" class="bold comment-label">备注:</span>
+         <span v-if="!isEditOrder" class="text-content">上次忘寄的化妆棉寄过去</span>
+         <custInput v-if="isEditOrder" :type="'textarea'" class="commentInput" :placeholder="'备注'">
+       </div>
+       <div class="payWraper">
+        <span class="edit" v-if="!isEditOrder">付款方式:</span>
+        <span class="text-content" v-if="!isEditOrder">微信支付</span>
+        <span v-if="isEditOrder"><custInput class="payInput" :placeholder="'付款方式/状态'"></span>
        </div>
        <div class="actionWraper">
-	    <span class="text-nav clickable edit" v-if="!isEdit">编辑</span>
-	    <span class="text-nav clickable edit" v-if="isEdit">保存</span>
-	    <span class="text-nav clickable cancel" v-if="isEdit">取消</span>        
+        <span class="text-nav clickable edit" @click="editOrder" v-if="!isEditOrder">编辑</span>
+	    <span class="text-nav clickable edit" @click="saveEditOrder" v-if="isEditOrder">保存</span>
+	    <span class="text-nav clickable cancel" @click="cancelEditOrder" v-if="isEditOrder">取消</span> 
+	    <span class="text-nav clickable cancel" v-if="!isEdit">结束订单</span>       
        </div>
       </div>
      </div><!--orderDetailContainer-->
@@ -111,12 +130,40 @@ export default {
   data () {
     return {
       isEdit:false,
-      isCollapse:this.collapse
+      isCollapse:this.collapse,
+      addingProduct:false,
+      isEditOrder: false
     }
   },
 
   components: {
     CustInput
+  },
+
+  methods: {
+   addNew() {
+    this.addingProduct = true
+   },
+
+   saveNew() {
+    this.addingProduct = false
+   },
+
+   cancelNew() {
+    this.addingProduct = false
+   },
+
+   editOrder() {
+    this.isEditOrder = true
+   },
+
+   cancelEditOrder() {
+    this.isEditOrder = false
+   },
+
+   saveEditOrder() {
+    this.isEditOrder = false
+   }
   }
 
 }
@@ -179,22 +226,25 @@ export default {
   	  text-align: left;
   	  padding: 0 10px;
 
-  	  &:nth-child(1){width:30%;}
-  	  &:nth-child(2){width:20%;}
-  	  &:nth-child(3){width:14%;}
-  	  &:nth-child(4){width:13%;}  
-  	  &:nth-child(5){width:13%;}
-      &:nth-child(6){width:10%; text-align:center;}      
+  	  &:nth-child(1){width:35%;}
+  	  &:nth-child(2){width:10%;}
+  	  &:nth-child(3){width:10%;}
+  	  &:nth-child(4){width:15%;}  
+  	  &:nth-child(5){width:15%;}
+      &:nth-child(6){width:15%;}      
   	 }
 
      & thead{
       & tr td{
        height: 45px;
+       padding-left:10px;
+       border-bottom: 2px solid lighten(black,70%);
+
+       &:last-child{
+        text-align: center;
+       }
       }
 
-      & tr:last-child td{
-       border-bottom: 2px solid lighten(black,70%);
-      }
      }/*end thead*/
 
      & tbody{
@@ -204,11 +254,37 @@ export default {
 
 	  & tr td{
 	    height: 45px;
+	    padding-left:20px;
 	    border-top:2px solid #F6F7FA;
+
+  	   &:nth-child(6){text-align:center;}
 	  }
 
 	  & tr:last-child td{
 	    border-top:2px solid lighten(black,60%); 
+	  }
+
+	  & tr.newItemWraper{
+       
+       & td{
+         background:white;
+         height: 55px;      
+       }
+
+       & td:not(:last-child){
+        vertical-align: bottom;
+        padding-bottom:5px;
+       }
+
+       & .newItem{
+        margin-bottom: 0;
+       }
+
+       & .dropdown .text-nav{
+           color:lighten(black,40%);
+           font-size: 14px;
+           font-weight: 400;
+       }
 	  }
      }/*end tbody*/
     }/*end table*/
@@ -217,36 +293,56 @@ export default {
      position:relative;
      display: flex;
      justify-content: space-between;
-     align-items:center;
+     align-items:flex-end;
      padding:20px 40px;
 
        & .commentWraper{
         display: flex;
         flex-direction: column;
         text-align: left;
-        width: 50%;
+        width: 35%;
+
+        & .commentInput{
+         margin-bottom: 0;
+        }
 
         & .comment-label{
          margin-bottom: 10px;
         }
        }
 
+       & .payWraper{
+        display: flex;
+        width:25%;
+        justify-content: center;
+        align-items:flex-end;
+        flex-wrap: wrap;
+
+        & > * {
+         margin: 0 .5em;
+        }
+
+        & .payInput{
+         width:130px;
+         margin-bottom: 0px;
+        }
+       }
+
 	   & .actionWraper{
+	    width: 40%;
 	    display: flex;
-	    padding-top:40px;
-	    padding-right:40px;
 	    justify-content: flex-end;
 	    align-items:center;
+
+	    & > * {
+	     margin:0 1em;
+	    }
 	   }/*end action*/
     }
   }/* end orderDetailContainer*/
 
   & .clickable:hover{
 	cursor: pointer;
-  }
-
-  & .edit, & .cancel{
-    margin-left:20px;
   }
 
   & .edit{
@@ -274,6 +370,45 @@ export default {
    0%  {transform: scaley(0)}
    50% {transform: scaley(1.2)}
    100 {transform: scaley(1)}
+  }
+
+  & .plus{
+    left:50%;
+    transform: translatex(-50%);
+    position: relative;
+    display: block;
+    width: 30px;
+    height: 30px;
+    padding: 0;
+    border: 1px solid #6ba045;
+    outline: none;
+    border-radius: 50%;
+    background: transparent; 
+
+    &:before{
+	    content: "";
+	    display: block;
+	    position: absolute;
+	    top: 50%;
+	    width: 60%;
+	    left: 20%;
+	    height: 2px;
+	    margin-top: -1px;
+	    background: #6ba045;
+    }
+
+    &:after{
+	    content: "";
+	    display: block;
+	    position: absolute;
+	    top: 50%;
+	    width: 60%;
+	    left: 20%;
+	    height: 2px;
+	    margin-top: -1px;
+	    background: #6ba045;
+	    transform: rotate(90deg);
+    }
   }
  }
 </style>
